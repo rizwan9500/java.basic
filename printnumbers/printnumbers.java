bb1315a3c5d2d0fc46a55c156f9f8e553f6c2308
Rizwan Ahmed A <rizwan240101@gmail.com>
2021-04-16 01:41:43 +0530

    printnumbers.java

--- printnumbers/printnumbers.java
+++ printnumbers/printnumbers.java
@@ -1,1 +1,18 @@
+package printnumbers;
 
+import java.io.*;
+import java.util.*;
+
+public class printnumbers {
+    public static void main(String[] args) {
+
+        int number;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the number");
+        number = scanner.nextInt();
+        System.out.println("Result:");
+        for (int i = 0; i <= number; i++) {
+            System.out.println(i);
+        }
+    }
+}
